e6019cdc065c101224508b63233a5a40113b8262
Jeffrey Yang <jeffycyang@gmail.com>
2016-02-11 17:02:44 -0800

    fixes priority queue

--- DataStructures/Queue/priorityqueue.js
+++ DataStructures/Queue/priorityqueue.js
@@ -13,6 +13,9 @@ PriorityQueue.prototype.enqueue=function(val,priority){
 		priority=0;
 	}
 	this.inbox.push([val,priority]);
+	while(this.outbox.length>0){
+		this.inbox.push(this.outbox.pop());
+	}
     this.inbox.sort(function(x,y){
         return y[1]-x[1];    
     });
@@ -23,6 +26,13 @@ PriorityQueue.prototype.inheritedDequeue=Queue.prototype.dequeue;
 PriorityQueue.prototype.dequeue=function(){
     var val=this.inheritedDequeue();
     return val[0];
+};
+
+PriorityQueue.prototype.peek=function(){
+	if(this.outbox.length){
+		return this.outbox[this.outbox.length-1][0];
+	}
+	return this.inbox[0][0];
 };
 
 PriorityQueue.prototype.size=Queue.prototype.size;
